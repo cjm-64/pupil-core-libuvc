8e91803af5ee116b65936af574dcfb36f8c844fd
Chris <christopher.morris7164@gmail.com>
2023-04-27 15:43:06 -0400

    Functions fully implemented. Need to work on why some frames are dropped

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@
 #include <opencv2/imgproc.hpp>
 
 using namespace std;
+using namespace cv;
 
 struct CamSettings{
     string CamName;
@@ -29,10 +30,8 @@ struct CamInfo{
     string CamName;
     uint8_t vID;
     uint8_t pID;
-//    int DevAddr;
-//    int BusNum;
     string uid;
-    int bus_loc;
+    int cam_num;
 };
 
 struct StreamingInfo{
@@ -41,6 +40,9 @@ struct StreamingInfo{
     uvc_device_handle_t *devh;
     uvc_stream_ctrl_t ctrl;
     uvc_stream_handle_t *strmh;
+    const uvc_format_desc_t *format_desc;
+    uvc_frame_desc_t *frame_desc;
+    enum uvc_frame_format frame_format;
 };
 
 void getCamInfo(struct CamInfo *ci){
@@ -79,7 +81,7 @@ void getCamInfo(struct CamInfo *ci){
                 ci[dev_struct_count].vID = desc->idVendor;
                 ci[dev_struct_count].pID = desc->idProduct;
                 ci[dev_struct_count].uid = to_string(uvc_get_device_address(dev))+":"+to_string(uvc_get_bus_number(dev));
-                ci[dev_struct_count].bus_loc = i;
+                ci[dev_struct_count].cam_num = i;
                 dev_struct_count++;
             }
             printf("Got desc\n");
@@ -109,18 +111,111 @@ void setUpStreams(struct CamSettings *cs, struct CamInfo *ci, struct StreamingIn
         else{
             cout << "Dev " << i << ": " << si[i].dev << endl;
         }
-        res = uvc_open(devicelist[ci[i].bus_loc], &si[i].devh, 1);
+        res = uvc_open(devicelist[ci[i].cam_num], &si[i].devh, 1);
         if (res < 0) {
             uvc_perror(res, "uvc_find_device"); /* no devices found */
         }
         else{
             cout << "devh " << i << ": " << si[i].devh << endl;
         }
+        si[i].format_desc = uvc_get_format_descs(si[i].devh);
+        si[i].frame_desc = si[i].format_desc->frame_descs->next;
+        si[i].frame_format = UVC_FRAME_FORMAT_ANY;
+        if(si[i].frame_desc->wWidth != NULL){
+            cs[i].width = si[i].frame_desc->wWidth;
+            cs[i].height = si[i].frame_desc->wHeight;
+            cs[i].fps = 10000000 / si[i].frame_desc->intervals[2];
+        }
+        printf("\nEye %d format: (%4s) %dx%d %dfps\n", i, si[i].format_desc->fourccFormat, cs[i].width, cs[i].height, cs[i].fps);
+
+        res = uvc_get_stream_ctrl_format_size(si[i].devh, &si[i].ctrl, si[i].frame_format, cs[i].width, cs[i].height, cs[i].fps, 1);
+        if (res < 0){
+            uvc_perror(res, "start_streaming");
+        }
+        else{
+            printf("Eye %d stream control formatted\n", i);
+            uvc_print_stream_ctrl(&si[i].ctrl, stderr);
+        }
+
+        res = uvc_stream_open_ctrl(si[i].devh, &si[i].strmh, &si[i].ctrl,1);
+        if (res < 0){
+            uvc_perror(res, "start_streaming");
+        }
+        else{
+            printf("Eye %d stream opened\n", i);
+        }
+
+        res = uvc_stream_start(si[i].strmh, nullptr, nullptr,2.0,0);
+        if (res < 0){
+            uvc_perror(res, "start_streaming");
+        }
+        else{
+            printf("Eye %d stream started\n", i);
+        }
         printf("\n\n\n");
     }
 }
 
-int main() {
+void callback(struct StreamingInfo *si){
+    uvc_frame_t *frame;
+    uvc_frame_t *bgr;
+    uvc_error_t res;
+
+    Mat image;
+
+    for (int i = 0; i<2;i++){
+        res = uvc_stream_get_frame(si[i].strmh, &frame, 1* pow(10,5));
+        if(res < 0){
+            uvc_perror(res, "Failed to get frame");
+        }
+        else{
+            printf("got frame");
+        }
+
+        //Allocate buffers for conversions
+        int frameW = frame->width;
+        int frameH = frame->height;
+        long unsigned int frameBytes = frame->data_bytes;
+
+        printf("callback! frame_format = %d, width = %d, height = %d, length = %lu\n",frame->frame_format, frameW, frameH, frameBytes);
+
+        cout << "Format: YUYV" << endl;
+        bgr = uvc_allocate_frame(frameW * frameH * 3);
+        if (!bgr) {
+            printf("unable to allocate bgr frame!\n");
+            return;
+        }
+        uvc_error_t res = uvc_yuyv2bgr(frame, bgr);
+        if (res < 0){
+            printf("Unable to copy frame to bgr!\n");
+        }
+        Mat placeholder(bgr->height, bgr->width, CV_8UC3, bgr->data);
+        placeholder.copyTo(image);
+        placeholder.release();
+        uvc_free_frame(bgr);
+
+        Mat adjusted;
+        flip(image, adjusted, 0);
+//    namedWindow(winname, WINDOW_AUTOSIZE);
+        printf("create win\n");
+        imshow(to_string(i),adjusted);
+        printf("img shown\n");
+        if(i == 0){
+            moveWindow(to_string(i), 200, 200);
+        }
+        else{
+            moveWindow(to_string(i), 450, 200);
+        }
+        waitKey(1);
+        printf("waitkeyed\n");
+        adjusted.release();
+        placeholder.release();
+        image.release();
+    }
+
+}
+
+int main(int argc, char* argv[]) {
     CamSettings camSets[2];
     for(int i = 0; i<2;i++){
         if(i == 0){
@@ -143,7 +238,27 @@ int main() {
     StreamingInfo CamStreams[2];
     setUpStreams(camSets, Cameras, CamStreams);
 
+    if (atoi(argv[1]) < 1){
+        printf("No run cam\n");
+    }
+    else{
+        int run_count = 0;
+        auto start = chrono::system_clock::now();
+        auto curr_time = chrono::system_clock::now();
+        chrono::duration<double> elapsed_seconds = curr_time-start;
+        while (elapsed_seconds.count() < atoi(argv[1])){
+            callback(CamStreams);
+            curr_time = chrono::system_clock::now();
+            elapsed_seconds = curr_time-start;
+            run_count = run_count + 1;
+        }
+        cout << "Total time: " << elapsed_seconds.count() << "s" << endl;
+        cout << "Total runs: " << run_count << endl;
+    }
 
+    for(int k = 0; k<2; k++){
+        uvc_exit(CamStreams[k].ctx);
+    }
 
     std::cout << "Hello, World!" << std::endl;
     return 0;
