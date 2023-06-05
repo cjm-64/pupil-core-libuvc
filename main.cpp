cfdcc04a494a49b0d5115db4bd536f062fc6f69f
Chris <61848401+cjm-64@users.noreply.github.com>
2023-06-05 10:01:01 -0400

    Update main.cpp to current

--- main.cpp
+++ main.cpp
@@ -15,317 +15,314 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 
-using namespace cv;
 using namespace std;
+using namespace cv;
 
-Mat image;
+//Circle
+Scalar col = Scalar(0, 255, 0); // green
 
-int run_count = 0;
-tjhandle _jpegDecompressor = tjInitDecompress();
+struct CamSettings{
+    string CamName;
+    int width;
+    int height;
+    int fps;
+};
 
-int getCamInfo (int i) {
+// ID0 = right, ID1 = Left, ID2 = World
+struct CamInfo{
+    string CamName;
+    uint8_t vID;
+    uint8_t pID;
+    string uid;
+    int cam_num;
+};
+
+struct StreamingInfo{
     uvc_context_t *ctx;
-    uvc_device_t **device_list;
+    uvc_device_t *dev;
     uvc_device_handle_t *devh;
     uvc_stream_ctrl_t ctrl;
+    uvc_stream_handle_t *strmh;
+    const uvc_format_desc_t *format_desc;
+    uvc_frame_desc_t *frame_desc;
+    enum uvc_frame_format frame_format;
+};
+
+void getCamInfo(struct CamInfo *ci){
+    uvc_context_t *ctx;
+    uvc_device_t **device_list;
+    uvc_device_t *dev;
+    uvc_device_descriptor_t *desc;
     uvc_error_t res;
 
-    printf("\n\n\n");
-    res = uvc_init(&ctx, NULL);
-    if (res < 0) {
+    res = uvc_init(&ctx,NULL);
+    if(res < 0) {
         uvc_perror(res, "uvc_init");
-    } else {
-        printf("ctx initialized\n");
     }
 
-    res = uvc_find_devices(ctx, &device_list, 0, 0, NULL);
+    res = uvc_get_device_list(ctx,&device_list);
     if (res < 0) {
-        uvc_perror(res, "uvc_find_device"); /* no devices found */
-    } else {
-        puts("Devices found");
+        uvc_perror(res, "uvc_find_device");
     }
-
-    res = uvc_open(device_list[i], &devh, 1);
-    if (res < 0) {
-        uvc_perror(res, "uvc_open"); /* unable to open device */
-        cout << i << " failed to open" << endl;
-    } else {
-        cout << i << " opened" << endl;
-//        uvc_print_diag(devh, stderr);
+    int i = 0;
+    int dev_struct_count = 0;
+    while (true) {
+        dev = device_list[i];
+        if (dev == NULL) {
+            break;
+        }
+        else{
+            uvc_get_device_descriptor(dev, &desc);
+            printf("Got desc\n");
+            if(desc->idVendor != 3141){
+                printf("World Cam\n");
+                i++;
+                continue;
+            }
+            else{
+                string name = desc->product;
+                if (name.substr(name.size()-1, 1) == "0"){
+                    cout << "Right Eye Camera: " << name << endl;
+                    dev_struct_count = 0;
+                }
+                else{
+                    cout << "Left Eye Camera: " << name << endl;
+                    dev_struct_count = 1;
+                }
+                ci[dev_struct_count].CamName = desc->product;
+                ci[dev_struct_count].vID = desc->idVendor;
+                ci[dev_struct_count].pID = desc->idProduct;
+                ci[dev_struct_count].uid = to_string(uvc_get_device_address(dev))+":"+to_string(uvc_get_bus_number(dev));
+                ci[dev_struct_count].cam_num = i;
+            }
+        }
+        uvc_free_device_descriptor(desc);
+        i++;
     }
-    int width, height, fps;
-
-    const uvc_format_desc_t *format_desc = uvc_get_format_descs(devh);
-    const uvc_frame_desc_t *frame_desc = format_desc->frame_descs->next;
-    enum uvc_frame_format frame_format = UVC_FRAME_FORMAT_ANY;
-    if (frame_desc) {
-        width = frame_desc->wWidth;
-        height = frame_desc->wHeight;
-        fps = 10000000 / frame_desc->intervals[2];
-    }
-    cout << i << ": ";
-    printf("(%4s) %dx%d %dfps\n", format_desc->fourccFormat, width, height, fps);
-
-    bool retval;
-    cout << "fps: " << fps << endl;
-    if (fps == 120) {
-        retval = true;
-    } else {
-        retval = false;
-    }
-
-    uvc_close(devh);
     uvc_exit(ctx);
-
-    return retval;
 }
 
-void callback(uvc_stream_handle_t *hand, string winname){
+void streamTests(struct CamSettings *cs, struct CamInfo *ci, struct StreamingInfo *si){
+    uvc_error_t res;
+    uvc_device_t **devicelist;
+    uvc_device_t *dev;
+    uvc_device_descriptor_t *desc;
+
+    for (int i = 0;i<2;++i) {
+        res = uvc_init(&si[i].ctx, NULL);
+        if (res < 0) {
+            uvc_perror(res, "uvc_init");
+        }
+        else{
+            printf("UVC %d open success\n", i);
+        }
+        res = uvc_find_devices(si[i].ctx, &devicelist, 0, 0, NULL);
+        if (res < 0) {
+            uvc_perror(res, "uvc_init");
+        }
+        for (int j = 0; j < 3; ++j){
+            dev = devicelist[j];
+            uvc_get_device_descriptor(dev, &desc);
+            cout << "Dev " << j << ": " << dev << " Name: " << desc->product << endl;
+        }
+        res = uvc_open(devicelist[ci[i].cam_num], &si[i].devh, 1);
+        if (res < 0) {
+            uvc_perror(res, "uvc_find_device"); /* no devices found */
+        }
+        else{
+            cout << "devh " << i << ": " << si[i].devh << endl;
+        }
+    }
+}
+
+void setUpStreams(struct CamSettings *cs, struct CamInfo *ci, struct StreamingInfo *si){
+    uvc_error_t res;
+    uvc_device_t **devicelist;
+    uvc_device_t *dev;
+    uvc_device_descriptor_t *desc;
+
+    for(int i = 0; i<2;++i){
+        res = uvc_init(&si[i].ctx, NULL);
+        if (res < 0) {
+            uvc_perror(res, "uvc_init");
+        }
+        else{
+            printf("UVC %d open success\n", i);
+        }
+        res = uvc_find_devices(si[i].ctx, &devicelist, 0, 0, NULL);
+        if (res < 0) {
+            uvc_perror(res, "uvc_init");
+        }
+        else{
+            for (int j = 0; j < 3; ++j){
+                dev = devicelist[j];
+                uvc_get_device_descriptor(dev, &desc);
+                cout << "   Dev " << j << ": " << dev << " Name: " << desc->product << endl;
+            }
+        }
+
+        res = uvc_open(devicelist[ci[i].cam_num], &si[i].devh, 1);
+        if (res < 0) {
+            uvc_perror(res, "uvc_find_device"); /* no devices found */
+        }
+        else{
+            cout << "devh " << i << ": " << si[i].devh << endl;
+            cout << "Name " << i << ": " << ci[i].CamName << endl;
+        }
+        si[i].format_desc = uvc_get_format_descs(si[i].devh);
+        si[i].frame_desc = si[i].format_desc->frame_descs->next;
+        si[i].frame_format = UVC_FRAME_FORMAT_ANY;
+        if(si[i].frame_desc->wWidth != NULL){
+            cs[i].width = si[i].frame_desc->wWidth;
+            cs[i].height = si[i].frame_desc->wHeight;
+            cs[i].fps = 10000000 / si[i].frame_desc->intervals[2];
+        }
+        printf("\nEye %d format: (%4s) %dx%d %dfps\n", i, si[i].format_desc->fourccFormat, cs[i].width, cs[i].height, cs[i].fps);
+
+        res = uvc_get_stream_ctrl_format_size(si[i].devh, &si[i].ctrl, si[i].frame_format, cs[i].width, cs[i].height, cs[i].fps, 1);
+        if (res < 0){
+            uvc_perror(res, "start_streaming");
+        }
+        else{
+            printf("Eye %d stream control formatted\n", i);
+            uvc_print_stream_ctrl(&si[i].ctrl, stderr);
+        }
+        res = uvc_stream_open_ctrl(si[i].devh, &si[i].strmh, &si[i].ctrl,1);
+        if (res < 0){
+            uvc_perror(res, "start_streaming");
+        }
+        else{
+            printf("Eye %d stream opened\n", i);
+        }
+        res = uvc_stream_start(si[i].strmh, nullptr, nullptr,1.6,0);
+        if (res < 0){
+            uvc_perror(res, "start_streaming");
+        }
+        else{
+            printf("Eye %d stream started\n", i);
+        }
+        printf("\n\n\n");
+    }
+}
+
+void callback(struct StreamingInfo *si){
     uvc_frame_t *frame;
-    uvc_frame_t *bgr;
+    uvc_frame_t *rgb;
     uvc_error_t res;
 
-    res = uvc_stream_get_frame(hand, &frame, 0);
-    if(res < 0){
-        uvc_perror(res, "Failed to get frame");
-    }
-    else{
-        printf("got frame");
-    }
+    Mat image;
 
-    //Allocate buffers for conversions
-    int frameW = frame->width;
-    int frameH = frame->height;
-    long unsigned int frameBytes = frame->data_bytes;
+    for (int i = 0; i<2;i++){
+        res = uvc_stream_get_frame(si[i].strmh, &frame, 1 * pow(10,6));
+        if(res < 0){
+            uvc_perror(res, "Failed to get frame");
+            continue;
+        }
+        else{
+            printf("got frame");
+        }
 
-    printf("callback! frame_format = %d, width = %d, height = %d, length = %lu\n",frame->frame_format, frameW, frameH, frameBytes);
+        //Allocate buffers for conversions
+        int frameW = frame->width;
+        int frameH = frame->height;
+        long unsigned int frameBytes = frame->data_bytes;
 
-    if (frame->frame_format == 3){
-        // yuyv format
-        cout << "Format: YUYV" << endl;
-        bgr = uvc_allocate_frame(frameW * frameH * 3);
-        if (!bgr) {
+        printf("Eye %d: frame_format = %d, width = %d, height = %d, length = %lu\n", i, frame->frame_format, frameW, frameH, frameBytes);
+
+        rgb = uvc_allocate_frame(frameW * frameH * 3);
+        if (!rgb) {
             printf("unable to allocate bgr frame!\n");
             return;
         }
-        uvc_error_t res = uvc_yuyv2bgr(frame, bgr);
+        uvc_error_t res = uvc_yuyv2rgb(frame, rgb);
         if (res < 0){
             printf("Unable to copy frame to bgr!\n");
         }
-        Mat placeholder(bgr->height, bgr->width, CV_8UC3, bgr->data);
-        placeholder.copyTo(image);
-        placeholder.release();
-        uvc_free_frame(bgr);
-    }
-    else if (frame->frame_format == 7){
-        //jpeg format
-        cout << "Format: JPEG" << endl;
-        int status, j_width, j_height, jpegSubsamp, header_ok;
-        int colorComponents = 3;
-        unsigned char decompBuffer[frameW*frameH*colorComponents];
-        tjDecompressHeader2(_jpegDecompressor, (unsigned char *)frame->data, frameBytes,  &j_width, &j_height, &jpegSubsamp);
-        tjDecompress2(_jpegDecompressor, (unsigned char *)frame->data, frameBytes, decompBuffer, frameW, 0, frameH, TJPF_RGB, TJFLAG_FASTDCT);
-        Mat placeholder(Size(frameW,frameH), CV_8UC3, decompBuffer);
-        placeholder.copyTo(image);
-        cout << "dRows: " << image.rows << " dCols: " << image.cols << endl;
-        cout << "test " << decompBuffer[0] << endl;
-        placeholder.release();
-    }
-    else {
-        //Not one of the 2 formats, not sure how we got here tbh
-    }
 
-    Mat adjusted;
-    flip(image, adjusted, 0);
-//    namedWindow(winname, WINDOW_AUTOSIZE);
-    printf("create win\n");
-    imshow(winname,adjusted);
-    printf("img shown\n");
-    waitKey(1);
-    printf("waitkeyed\n");
-    adjusted.release();
+        // Iterate from CV_8UC1 to CV_8UC4 and see which one works
+        Mat placeholder(rgb->height, rgb->width, CV_8UC3, rgb->data);
+        placeholder.copyTo(image);
 
+
+        Mat adjusted;
+        flip(image, adjusted, 0);
+
+        //Display image
+        imshow(to_string(i),adjusted);
+        printf("img shown\n");
+        if(i == 0){
+            moveWindow(to_string(i), 500, 200);
+            //Move right eye to right
+        }
+        else{
+            moveWindow(to_string(i), 200, 200);
+            //Move left eye to left
+        }
+        waitKey(1);
+        printf("waitkeyed\n");
+
+        //Free memory
+        adjusted.release();
+        image.release();
+        placeholder.release();
+        uvc_free_frame(rgb);
+    }
 }
 
 int main(int argc, char* argv[]) {
-    uvc_context_t *Rightctx, *Leftctx;
-    uvc_device_t *dev;
-    uvc_device_t **devicelist;
-    uvc_device_descriptor_t *desc;
-    uvc_device_handle_t *righteyecam, *lefteyecam;
-    uvc_stream_ctrl_t righteyectrl, lefteyectrl;
-    uvc_stream_handle_t *righteyestrmh, *lefteyestrmh;
-    uvc_error_t res;
-
-    int width, height, fps;
-
-    int loc = 0;
-    int device_iterators[2] = {0};
-    for (int i = 0; i<3; i++){
-        if (getCamInfo(i) > 0){
-            device_iterators[loc] = i;
-            loc++;
+    CamSettings camSets[2];
+    for(int i = 0; i<2;i++){
+        if(i == 0){
+            camSets[i].CamName = "Pupil Cam2 ID0";
         }
         else{
-            printf("World cam; ignoring\n");
+            camSets[i].CamName = "Pupil Cam2 ID1";
         }
-    }
-    for (int i = 0; i<2; i++){
-        cout << "DI: " << device_iterators[i] << endl;
-    }
-    printf("\n\n\n");
-
-    res = uvc_init(&Rightctx, NULL);
-    if (res < 0) {
-        uvc_perror(res, "uvc_init");
-        return res;
-    }
-    else{
-        printf("Right init\n");
-        res = uvc_find_devices(Rightctx, &devicelist, 0, 0, NULL);
-        if (res < 0) {
-            uvc_perror(res, "uvc_find_device"); /* no devices found */
-        }
-        else {
-            printf("Right found\n");
-            res = uvc_open(devicelist[device_iterators[0]], &righteyecam, 1);
-            if (res < 0) {
-                uvc_perror(res, "uvc_open"); /* unable to open device */
-            }
-            else {
-                printf("Right opened\n");
-                const uvc_format_desc_t *right_format_desc = uvc_get_format_descs(righteyecam);
-                const uvc_frame_desc_t *right_frame_desc = right_format_desc->frame_descs->next;
-                enum uvc_frame_format right_frame_format = UVC_FRAME_FORMAT_ANY;
-                if (right_frame_desc) {
-                    width = right_frame_desc->wWidth;
-                    height = right_frame_desc->wHeight;
-                    fps = 10000000 / right_frame_desc->intervals[2];
-                }
-                printf("\nRight eye format: (%4s) %dx%d %dfps\n", right_format_desc->fourccFormat, width, height, fps);
-                res = uvc_get_stream_ctrl_format_size(righteyecam, &righteyectrl,right_frame_format, width, height, fps, 1);
-                if (res < 0){
-                    uvc_perror(res, "start_streaming");
-                }
-                else{
-                    printf("Right stream ctrl formatted\n");
-                    printf("\n\nRight eye stream controls\n");
-                    uvc_print_stream_ctrl(&righteyectrl, stderr);
-                    res = uvc_stream_open_ctrl(righteyecam, &righteyestrmh, &righteyectrl, 1);
-                    if (res < 0){
-                        uvc_perror(res, "start_streaming");
-                    }
-                    else{
-                        printf("Right stream open ctrl\n");
-                        res = uvc_stream_start(righteyestrmh, nullptr, nullptr,2.0,0);
-                        if (res < 0){
-                            uvc_perror(res, "start_streaming");
-                        }
-                        else{
-                            printf("Right stream start\n");
-                            res = uvc_stream_start(righteyestrmh, nullptr, nullptr,2.0,0);
-                            if (res < 0){
-                                printf("Right Eye Stream Start Fail\n\n\n");
-                            }
-                            else{
-                                printf("Right Eye Stream Started\n\n\n");
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        camSets[i].width = 192;
+        camSets[i].height = 192;
+        camSets[i].fps = 120;
     }
 
-    res = uvc_init(&Leftctx, NULL);
-    if (res < 0) {
-        uvc_perror(res, "uvc_init");
-        return res;
-    }
-    else{
-        printf("Left init\n");
-        res = uvc_find_devices(Leftctx, &devicelist, 0, 0, NULL);
-        if (res < 0) {
-            uvc_perror(res, "uvc_find_device"); /* no devices found */
-        }
-        else {
-            printf("Left found\n");
-            res = uvc_open(devicelist[device_iterators[1]], &lefteyecam, 1);
-            if (res < 0) {
-                uvc_perror(res, "uvc_open"); /* unable to open device */
-            }
-            else {
-                printf("Left opened\n");
-                const uvc_format_desc_t *left_format_desc = uvc_get_format_descs(lefteyecam);
-                const uvc_frame_desc_t *left_frame_desc = left_format_desc->frame_descs->next;
-                enum uvc_frame_format left_frame_format = UVC_FRAME_FORMAT_ANY;
-                if (left_frame_desc) {
-                    width = left_frame_desc->wWidth;
-                    height = left_frame_desc->wHeight;
-                    fps = 10000000 / left_frame_desc->intervals[2];
-                }
-                printf("\nLeft eye format: (%4s) %dx%d %dfps\n", left_format_desc->fourccFormat, width, height, fps);
-                res = uvc_get_stream_ctrl_format_size(lefteyecam, &lefteyectrl,left_frame_format, width, height, fps, 1);
-                if (res < 0){
-                    uvc_perror(res, "start_streaming");
-                }
-                else{
-                    printf("Left stream ctrl formatted\n");
-                    printf("\n\nLeft eye stream controls\n");
-                    uvc_print_stream_ctrl(&lefteyectrl, stderr);
-                    res = uvc_stream_open_ctrl(lefteyecam, &lefteyestrmh, &lefteyectrl, 1);
-                    if (res < 0){
-                        uvc_perror(res, "start_streaming");
-                    }
-                    else{
-                        printf("Left stream open ctrl\n");
-                        res = uvc_stream_start(lefteyestrmh, nullptr, nullptr,2.0,0);
-                        if (res < 0){
-                            uvc_perror(res, "start_streaming");
-                        }
-                        else{
-                            printf("Left stream start\n");
-                            res = uvc_stream_start(lefteyestrmh, nullptr, nullptr,2.0,0);
-                            if (res < 0){
-                                printf("Left Eye Stream Start Fail\n\n\n");
-                            }
-                            else{
-                                printf("Left Eye Stream Started\n\n\n");
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    CamInfo Cameras[2];
+    getCamInfo(Cameras);
+    for (int j = 0; j<2; j++){
+        cout << "Cam " << j << ": " << Cameras[j].CamName << " " << Cameras[j].uid << " " << Cameras[j].vID << " " << Cameras[j].pID << " " << Cameras[j].cam_num << endl;
     }
 
-
-    printf("Right\n");
-    uvc_print_diag(lefteyecam, stderr);
-    printf("Left\n");
-    uvc_print_diag(lefteyecam, stderr);
-
+    StreamingInfo CamStreams[2];
+    setUpStreams(camSets, Cameras, CamStreams);
 
     if (atoi(argv[1]) < 1){
         printf("No run cam\n");
     }
     else{
+        int run_count = 0;
         auto start = chrono::system_clock::now();
         auto curr_time = chrono::system_clock::now();
         chrono::duration<double> elapsed_seconds = curr_time-start;
         while (elapsed_seconds.count() < atoi(argv[1])){
-            callback(righteyestrmh, "Right Eye");
-            callback(lefteyestrmh, "Left Eye");
+            callback(CamStreams);
             curr_time = chrono::system_clock::now();
             elapsed_seconds = curr_time-start;
             run_count = run_count + 1;
         }
         cout << "Total time: " << elapsed_seconds.count() << "s" << endl;
         cout << "Total runs: " << run_count << endl;
+        cout << "Avg fps: " << run_count/atoi(argv[1]) << endl;
     }
 
-    uvc_exit(Rightctx);
-    uvc_exit(Leftctx);
+    for(int l = 0; l<2; ++l){
+        cout << "Stream " << l << ":" << endl;
+        cout << "   " << CamStreams[l].ctx << endl;
+        cout << "   " << CamStreams[l].devh << endl;
+        cout << "   " << &CamStreams[l].ctrl << endl;
+        cout << "   " << CamStreams[l].strmh << endl;
+        printf("\n\n");
+    }
+
+    for(int k = 0; k<2; k++){
+        uvc_exit(CamStreams[k].ctx);
+    }
 
     std::cout << "Hello, World!" << std::endl;
     return 0;
